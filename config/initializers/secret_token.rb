567dd99266fe2e85bdac746243c72fa9d5362eaf
Hélio Santos <heliosantos99@gmail.com>
2013-07-06 18:53:11 +0100

    Add rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bkb::Application.config.secret_token = '3ea4ac46f4b6b796951e01ae3081f476be51fa1b3238a7e039ae850cf00fac4e165de45c952bcecd1cde1d5c531fd6a8786390e354988f840b1f3079993f6eae'
