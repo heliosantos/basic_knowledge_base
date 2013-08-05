3e0c7fd116a577a8c06a82e15ae60376dbc20dd7
Helio Santos <heliosantos99@gmail.com>
2013-08-05 23:40:08 +0100

    Add a delete crud log for modified permalinks

--- app/models/article.rb
+++ app/models/article.rb
@@ -13,17 +13,17 @@ class Article
   def save
     self.permalink = self.title.to_slug
     super
-    CrudLogger.log_operation(self.permalink, 'created')
+    CrudLogger.log_operation(self._id, self.permalink, 'created')
   end
   
   def update_attributes(attributes = [])
     super(attributes)
-    CrudLogger.log_operation(self.permalink, 'updated')
+    CrudLogger.log_operation(self._id, self.permalink, 'updated')
   end
   
   def destroy
     super    
-    CrudLogger.log_operation(self.permalink, 'deleted')
+    CrudLogger.log_operation(self._id, self.permalink, 'deleted')
   end
   
   search_in :title, :body
